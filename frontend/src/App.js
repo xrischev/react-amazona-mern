c40f7c4d9f14030e97cc5f86bb5e5ab34936e7b3
hrischev <hrischev123@abv.bg>
2023-02-01 21:35:08 +0200

    create-react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+          <a href="/">amazona</a>
+      </header>
+        <main>
+            list products
+        </main>
+    </div>
+  );
+}
+
+export default App;
